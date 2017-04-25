3446008cf5323e8e7357f1090d6abd3d720e4c71
Waleed <humayed.waleed@gmail.com>
2017-04-25 16:57:28 +0300

    - added comments to config.development

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,11 +1,15 @@
 var logger = require('./logger.js');
 
 module.exports = {
+  // database name
+  // phpmyadmin's username & password
+  // root root
   database: "team",
-  username: "root",
-  password: "root",
+  username: "waleed",
+  password: "waleed",
   params: {
-    port: 8889,//3306
+    // mac:8889, Windows:3306
+    port: 3306,
     dialect: "mysql",
     logging: (sql) => {
       logger.info(`[${new Date()}] ${sql}`);
